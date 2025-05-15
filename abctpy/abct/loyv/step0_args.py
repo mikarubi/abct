065f2da307532285a4896997c9e405216d6370fd
mikarubi <mikarubi@gmail.com>
2025-05-15 00:25:01 -0500

    first pass of loyvain.py

--- abctpy/abct/loyv/step0_args.py
+++ abctpy/abct/loyv/step0_args.py
@@ -1,6 +1,7 @@
 from typing import Literal
 from numpy.typing import ArrayLike
 from pydantic import validate_call, ConfigDict
+from types import SimpleNamespace
 
 import numpy as np
 
@@ -15,15 +16,15 @@ def step0_args(method: str, *args, **kwargs) -> dict:
             n_args_num = 2
             W, k = args[:n_args_num]
             X = W
-            Y = 0
+            Y = [0]
         case "coloyvain":
-            n_args_num = 2 + (n_args > 2 and isinstance(args[2], (int, float)))
+            n_args_num = 2 + (n_args > 2 and (not isinstance(args[2], str)))
             if n_args_num == 2:
                 W, k = args[:n_args_num]
-                X, Y = 0, 0
+                X, Y = [0], [0]
             elif n_args_num == 3:
                 X, Y, k = args[:n_args_num]
-                W = 0
+                W = [0]
     args = args[n_args_num:]
     if n_args >= n_args_num + 1:
         kwargs["objective"] = args[0]
@@ -32,29 +33,36 @@ def step0_args(method: str, *args, **kwargs) -> dict:
             kwargs["similarity"] = args[0]
             args = args[1:]
 
-    return parse_args(method=method, W=W, X=X, Y=Y, k=k, **kwargs)
+    Args = parse_args(method=method, W=W, X=X, Y=Y, k=k, **kwargs)
+
+    return SimpleNamespace(**Args)
 
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def parse_args(
     method: Literal["loyvain", "coloyvain"],
-    W: ArrayLike = 0,
-    X: ArrayLike = 0,
-    Y: ArrayLike = 0,
+    W: ArrayLike = [0],
+    X: ArrayLike = [0],
+    Y: ArrayLike = [0],
     k: int = 0,
     objective: Literal["kmodularity", "kmeans", "spectral"] = "kmodularity",
     similarity: Literal["network", "corr", "cosim", "cov", "dot"] = "network",
-    start: Literal["greedy", "balanced", "random"] = "greedy",
+    start: ArrayLike | Literal["greedy", "balanced", "random"] = "greedy",
     numbatches: int = 10,
     maxiter: int = 1000,
     replicates: int = 10,
     tolerance: float = 1e-10,
     display: Literal["none", "replicate", "iteration"] = "none",
 ) -> dict:
+    
+    W = np.asarray(W)
+    X = np.asarray(X)
+    Y = np.asarray(Y)
 
-    if isinstance(start, (int, float)) and method == "loyvain":
+    if start in ["greedy", "balanced", "random"]:
         pass
-    elif isinstance(start, str) and start in ["greedy", "balanced", "random"]:
+    elif method == "loyvain":
+        start = np.asarray(start)
         pass
     else:
         raise ValueError('Start must be either "greedy", "balanced", "random", or a numeric vector (loyvain only).')
@@ -63,11 +71,11 @@ def parse_args(
         if k <= 0:
             raise ValueError("k must be positive for co-Loyvain.")
         if similarity == "network":
-            if not (np.array_equal(X, 0) and np.array_equal(Y, 0)):
-                raise ValueError("X and Y inputs are incompatible with 'network' similarity.")
+            if not (np.array_equal(X, [0]) and np.array_equal(Y, [0])):
+                raise ValueError('X and Y inputs are incompatible with "network" similarity.')
         else:
-            if not np.array_equal(W, 0):
-                raise ValueError("W input is only compatible with 'network' similarity.")
+            if not np.array_equal(W, [0]):
+                raise ValueError('W input is only compatible with "network" similarity.')
             if X.shape[0] != Y.shape[0]:
                 raise ValueError("X and Y must have the same number of data points.")
 
