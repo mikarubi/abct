d46d09f7d63462edf3a8153130cbb51aadaf06b2
mikarubi <mikarubi@gmail.com>
2025-05-20 08:56:31 -0500

    streamline tools

--- abct-matlab/matpackage.m
+++ abct-matlab/matpackage.m
@@ -1,4 +1,4 @@
-version = "1.0.0"
+version = "1.0.1"
 abct_dir = fullfile(fileparts(mfilename("fullpath")), "abct");
 delete(fullfile(abct_dir, "resources", "mpackage.json"));
 mpmcreate("abct", abct_dir, DisplayName="abct", ...
