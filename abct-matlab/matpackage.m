67a592ed76b5db15fe0f5892a5eb0d8878962464
mikarubi <mikarubi@gmail.com>
2025-05-20 00:11:18 -0500

    matpackage and update versions

---
+++ abct-matlab/matpackage.m
@@ -0,0 +1,5 @@
+version = "1.0.0"
+abct_dir = fullfile(fileparts(mfilename("fullpath")), "abct");
+delete(fullfile(abct_dir, "resources", "mpackage.json"));
+mpmcreate("abct", abct_dir, DisplayName="abct", ...
+    Version=version, ReleaseCompatibility=">=R2020b");
