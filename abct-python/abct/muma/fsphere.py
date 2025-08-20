201c277eaf1ef5f8efb554d46acf41f1aa517a2e
mikarubi <mikarubi@gmail.com>
2025-08-20 13:09:32 -0500

    test

---
+++ abct-python/abct/muma/fsphere.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def fsphere(p):
+    # Unit Fibonacci sphere
+    I = np.r_[:p] + 0.5
+    golden_ratio = (1 + np.sqrt(5))/2
+    Phi = np.acos(1 - 2*I/p)
+    Theta = 2*np.pi*I / golden_ratio
+    V = np.hstack([np.cos(Theta)*np.sin(Phi), np.sin(Theta)*np.sin(Phi), np.cos(Phi)])
+
+    return V
+    
