17cac70ff133c0b7b58385fd25185f99ed809eb8
mikarubi <mikarubi@gmail.com>
2025-08-24 17:05:18 -0500

    minor clean up

--- abct-matlab/abct/+muma/fsphere.m
+++ abct-matlab/abct/+muma/fsphere.m
@@ -4,6 +4,8 @@ I = (0:p-1).' + 0.5;
 golden_ratio = (1 + sqrt(5))/2;
 Phi = acos(1 - 2*I/p);
 Theta = 2*pi*I / golden_ratio;
-V = [cos(Theta).*sin(Phi), sin(Theta).*sin(Phi), cos(Phi)];
+V = [   cos(Theta).*sin(Phi), ...
+        sin(Theta).*sin(Phi), ...
+        cos(Phi)];
 
 end
