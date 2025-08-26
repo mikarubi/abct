debf98dc9df9f5a971b58fe506a30b11f2acf276
mikarubi <mikarubi@gmail.com>
2025-08-26 00:19:44 -0500

    m-umap fixes

--- abct-python/abct/leiden.py
+++ abct-python/abct/leiden.py
@@ -2,22 +2,21 @@ from typing import Literal
 from numpy.typing import ArrayLike
 from pydantic import validate_call, ConfigDict
 from importlib import resources
+from scipy.sparse import spmatrix
 
 import numpy as np
 import igraph as ig
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
-def louvains(
-    W: ArrayLike,
+def leiden(
+    W,
+    start,
     gamma: float = 1,
-    start: ArrayLike = None,
     replicates: int = 10,
     finaltune: bool = False,
     tolerance: float = 1e-10,
     display: Literal["none", "replicate"] = "none",
 ):
-    if np.any(W < 0) or not ((W.shape[0] == W.shape[1]) and np.allclose(W, W.T)):
-        raise ValueError("Matrix must be nonnegative and symmetric.")
 
     G = ig.Graph.Weighted_Adjacency(W, mode="undirected", attr="weight")
     Q = -np.inf
@@ -30,7 +29,6 @@ def louvains(
             weights="weight",
             resolution=gamma,
             initial_membership=start,
-            n_iterations=-1,
         )
         if finaltune:
             C = G.community_leiden(
@@ -38,7 +36,6 @@ def louvains(
                 weights="weight",
                 resolution=gamma,
                 initial_membership=C.membership,
-                n_iterations=-1,
             )
 
         # test for increase
@@ -51,4 +48,4 @@ def louvains(
 
     return M, Q
 
-louvains.__doc__ = resources.read_text("abct.docstrings", "louvains")
+leiden.__doc__ = resources.read_text("abct.docstrings", "louvains")
