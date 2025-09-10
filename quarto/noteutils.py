b35a996d42e4ce4ce39d82124f2cb5b3bf954531
mikarubi <mikarubi@gmail.com>
2025-09-10 18:21:35 -0500

    streamline notebooks

---
+++ quarto/noteutils.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy import io
+from nilearn import plotting
+import plotly.express as px
+
+data = io.loadmat("./data/hcp_data.mat")
+not_eye = ~np.eye(data["W"].shape[0], dtype=bool)
+
+# Get data
+W = data["W"]
+X = data["X"]
+C = X.T @ X
+Distance = data["D"]
+ordw = data["ordw"].flatten() - 1
+ordc = data["ordc"].flatten() - 1
+
+fig_scatter = lambda x, y, xl=None, yl=None, tl=None: \
+    px.scatter(x=x, y=y).\
+    update_layout(
+        xaxis_title=xl,
+        yaxis_title=yl, 
+        title=tl, 
+        title_x=0.5, 
+        width=600, 
+        height=600
+    )
+
+fig_imshow = lambda W, tl, cmap, pmin=5, pmax=95: \
+    px.imshow(W,
+        color_continuous_scale=cmap,
+        zmin=np.percentile(W, pmin) if pmax >= 0 else -pmin,
+        zmax=np.percentile(W, pmax) if pmax >= 0 else -pmax).\
+    update_layout(
+        title=tl,
+        title_x=0.5,
+        width=600,
+        height=600
+    )
+
+parc = data["parc"].ravel() - 1
+fig_surf = lambda M, tl, cmap, pmin=0, pmax=100: \
+    plotting.plot_surf(
+        surf_mesh="./data/S1200.L.very_inflated_MSMAll.32k_fs_LR.surf.gii",
+        surf_map=M[parc][:parc.size // 2],
+        cmap=cmap,
+        vmin=np.percentile(M[parc][:parc.size // 2], pmin),
+        vmax=np.percentile(M[parc][:parc.size // 2], pmax),
+        title=tl,
+        view="lateral",
+    )
